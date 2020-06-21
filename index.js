6a2e77ec9f403319a53f838feed01843d3544e38
Laura Berge <lauraeberge@gmail.com>
2020-06-21 16:20:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function findMinAndRemove(array){
-
+    let min = Math.min(...array)
+    array.splice(array.indexOf(min), 1)
+    return min
 }
 
 function insertionSort(array){
-
+    let sorted = []
+    while (array.length > 0) {
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted
 }
